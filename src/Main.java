cf5cd9373ce35f1e239b39945503e83aff10d96d
Irina Dolgova <claycharm@yandex.ru>
2022-02-19 21:52:43 +0300

    miles

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int price_mile = 20;
+        int ticket = 6000;
+        int mile = ticket / price_mile;
+        System.out.println("Бонусные мили");
+        System.out.println (mile);
+
+    }
+}
