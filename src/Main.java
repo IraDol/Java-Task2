0fb62b59bf5996ec8523213bf3870515e09043ef
Irina Dolgova <claycharm@yandex.ru>
2022-02-23 13:53:47 +0300

    add src

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        int price_mile = 20;
+        int priceMile = 20;
         int ticket = 6000;
-        int mile = ticket / price_mile;
+        int mile = ticket / priceMile;
         System.out.println("Бонусные мили");
         System.out.println(mile);
 
